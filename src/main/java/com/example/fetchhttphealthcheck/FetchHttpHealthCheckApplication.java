43ec968d9d87ee60dbad994e5fbf568a5a6c9266
Parikshit Narang <parikshitnarang@Parikshits-MacBook-Pro.local>
2023-12-20 18:31:25 -0500

    initialize spring repository

---
+++ src/main/java/com/example/fetchhttphealthcheck/FetchHttpHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.fetchhttphealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchHttpHealthCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchHttpHealthCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fetchhttphealthcheck/FetchHttpHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fetchhttphealthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchHttpHealthCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
